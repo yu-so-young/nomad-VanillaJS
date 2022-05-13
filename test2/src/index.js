013971407d993a4a94d4372a5a50967b8d0b90a7
Yu soyoung <sy980911@gmail.com>
2022-05-13 14:32:50 +0900

    5일차

--- test2/src/index.js
+++ test2/src/index.js
@@ -1,17 +1,29 @@
+// 브라우저 너비에 따라 background 색상이 달라지는 웹페이지
+
 function handleWindowResize() {
   const windowSize = window.innerWidth;
   if(windowSize <= 300) {
-    body.className = "narrow";
+    body.classList.add("narrow");
+    if(body.classList.contains("wide")) {
+      body.classList.remove("wide");
+    }
   }
   else if(windowSize >= 800) {
-    body.className = "wide";
+    body.classList.add("wide");
+    if(body.classList.contains("narrow")) {
+      body.classList.remove("narrow");
+    }
   }
   else {
-    body.className = "default";
+    if(body.classList.contains("narrow")) {
+      body.classList.remove("narrow");
+    }
+    if(body.classList.contains("wide")) {
+      body.classList.remove("wide");
+    }
   }
   
 };
 
-alert("haha");
 const body = document.body;
 window.addEventListener("resize", handleWindowResize)
