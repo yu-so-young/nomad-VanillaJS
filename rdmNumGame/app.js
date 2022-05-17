e5ce528621d7a2e8e137dfc9f1bc4e42a6220646
Yu soyoung <sy980911@gmail.com>
2022-05-17 20:55:12 +0900

    8일차

--- rdmNumGame/app.js
+++ rdmNumGame/app.js
@@ -1,47 +1,26 @@
+const USERMAX_KEY = "maxnum";
+const USERINPUT_KEY = "userinput";
 
-const HIDDEN_CLASSNAME = "hidden" // 같은 string 을 2번 이상 사용할 때는 저장해서 쓰기, 중요한 정보가 아니니까 그냥 대문자로
-const USERNAME_KEY = "username";
+const maxForm = document.querySelector("#game #usermax");
+const maxNum = maxForm.querySelector("input");
+const userForm = document.querySelector("#game #userinput");
+const userInput = userForm.querySelector("input");
+const playBtn = document.querySelector("#game #userinput button");
 
-const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input"); // element 에서 각 element 가져오기
-const greeting = document.querySelector("#greeting");
-
-function onLoginSubmit(event) {
-    event.preventDefault(); // 브라우저의 기본 동작(이 경우엔 새로고침)을 막아줌
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+function onMaxSubmit(event){
+    event.preventDefault();
+    console.log("gi");
+    localStorage.setItem(USERMAX_KEY, maxNum); // 저장
+    localStorage.setItem(USERINPUT_KEY, userInput); // 저장
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}!`; // 이게 더 보기 좋아
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
+const savedUserMax = localStorage.getItem(USERMAX_KEY);
+const savedUserInput = localStorage.getItem(USERINPUT_KEY);
 
-const savedUsername = localStorage.getItem(USERNAME_KEY); // username 은 submit 되었을 때만 생기는 변수
+if(savedUserMax === null){
+    //get
+    playBtn.addEventListener("click", onMaxSubmit);
 
-if (savedUsername === null) {
-    // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    // show the greetings 
-    paintGreetings(savedUsername);
- }
-
-
-
-
-
-// 새로고침 안하게 하려면? -> preventDefault
-// 모든 EventListener function의 첫번째 argument 는 항상 현재 벌어진 일들에 대한 정보
-
-// form 제출 후 form 이 사라지게 하고 싶다면?
-// 1) html element 자체를 없애기
-// 2) css 이용
-
-// user를 기억하는 방법은? 새로고침 할 때마다 새로 로그인 해야 하잖아
-// form 보여주기 전에 user 가 로그인했는지 안했는지 확인
-// local storage 에 user 있으면 form 보여주지 말기
-// 어떻게 체크? -> localStorage.getItem()
+    // play game
+}
