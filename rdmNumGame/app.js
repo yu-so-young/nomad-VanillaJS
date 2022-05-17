1f8eee9656c8b5be813c4ebcb97419e360390d07
Yu soyoung <sy980911@gmail.com>
2022-05-18 01:02:18 +0900

    8일차 pjt

--- rdmNumGame/app.js
+++ rdmNumGame/app.js
@@ -1,26 +1,47 @@
-const USERMAX_KEY = "maxnum";
-const USERINPUT_KEY = "userinput";
+const MAXNUM_KEY = "maxNumInput";
+const USERNUM_KEY = "userNumInput";
 
-const maxForm = document.querySelector("#game #usermax");
-const maxNum = maxForm.querySelector("input");
-const userForm = document.querySelector("#game #userinput");
-const userInput = userForm.querySelector("input");
+const maxNumInput = document.querySelector("#game #usermax input");
+const userNumInput = document.querySelector("#game #userinput input");
 const playBtn = document.querySelector("#game #userinput button");
 
-function onMaxSubmit(event){
-    event.preventDefault();
-    console.log("gi");
-    localStorage.setItem(USERMAX_KEY, maxNum); // 저장
-    localStorage.setItem(USERINPUT_KEY, userInput); // 저장
+function clickPlayBtn(event) {
+  event.preventDefault();
+  console.log("gi");
+  const maxNum = maxNumInput.value;
+  const userNum = userNumInput.value;
+
+  showResult(userNum, maxNum);
 }
 
-const savedUserMax = localStorage.getItem(USERMAX_KEY);
-const savedUserInput = localStorage.getItem(USERINPUT_KEY);
-
-if(savedUserMax === null){
-    //get
-    playBtn.addEventListener("click", onMaxSubmit);
-
-} else {
-    // play game
+function showResult(input, max) {
+  let random = genRanNum(max);
+  let result = "win";
+  if (judge(input, random) === false) {
+    result = "lost";
+  }
+  result1.classList.remove("hidden");
+  result2.classList.remove("hidden");
+  result1.innerText = `You chose ${input}, the machine chose ${random}.`;
+  result2.innerText = `You ${result}!`;
 }
+
+function genRanNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function judge(input, answer) {
+  if (input == answer) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const savedUserMax = localStorage.getItem(MAXNUM_KEY);
+const savedUserInput = localStorage.getItem(USERNUM_KEY);
+
+const result1 = document.querySelector("#game #userinput #result1");
+const result2 = document.querySelector("#game #userinput #result2");
+
+playBtn.addEventListener("click", clickPlayBtn);
